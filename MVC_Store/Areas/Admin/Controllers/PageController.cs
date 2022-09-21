d27cdccc8dc88a9658525c6bfe0ecc6a222e2447
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-09-21 18:20:20 +0300

    Добавьте файлы проекта.

---
+++ MVC_Store/Areas/Admin/Controllers/PageController.cs
@@ -0,0 +1,85 @@
+﻿using MVC_Store.Models.Data;
+using MVC_Store.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Store.Areas.Admin.Controllers
+{
+    public class PageController : Controller
+    {
+        // GET: Admin/Page
+        public ActionResult Index()
+        {
+            List<PageVM> pageList;
+
+            using (Db db = new Db())
+            {
+                pageList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            return View(pageList);
+        }
+
+        // GET: Admin/Page/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+
+        // POST: Admin/Page/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (var db = new Db())
+            {
+                string slug;
+
+                var dto = new PagesDTO();
+
+                dto.Title = model.Title.ToUpper();
+
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower(); 
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                if (db.Pages.Any(x => x.Title == model.Title))
+                {
+                    ModelState.AddModelError("", "That title already exist.");
+                    return View(model);
+                }
+                else if(db.Pages.Any(x => x.Slug == model.Slug))
+                {
+                    ModelState.AddModelError("", "That slug already exist.");
+                    return View(model);
+                }
+
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "You have added a new page";
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
