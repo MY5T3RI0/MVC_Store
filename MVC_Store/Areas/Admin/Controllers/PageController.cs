7120c774f36197391ebf44aaa478f9a940e1cd15
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-09-24 20:57:43 +0300

    Добавлено редактирование страниц и детали

--- MVC_Store/Areas/Admin/Controllers/PageController.cs
+++ MVC_Store/Areas/Admin/Controllers/PageController.cs
@@ -81,5 +81,100 @@ namespace MVC_Store.Areas.Admin.Controllers
             return RedirectToAction("Index");
 
         }
+
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+            PageVM model;
+            using(var db = new Db())
+            {
+                var dto = db.Pages.Find(id);
+
+                if (dto == null)
+                {
+                    return Content("That page has not exist.");
+                }
+                model = new PageVM(dto);  
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var id = model.Id;
+
+            using (var db = new Db())
+            {
+                var dto = db.Pages.Find(id);
+
+                if (dto == null)
+                {
+                    return Content("That page has not exist.");
+                }
+
+                dto.Title = model.Title;
+
+                string slug = "home";
+
+                if (model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ", "-").ToLower();
+                    }
+                    else
+                    {
+                        slug = model.Slug.Replace(" ", "-").ToLower();
+                    }
+                }
+
+                if (db.Pages.Where(p => p.Id != id).Any(p => p.Title == model.Title))
+                {
+                    ModelState.AddModelError("","This title has already exist");
+                    return View(model);
+                }
+                else if (db.Pages.Where(p => p.Id != id).Any(p => p.Slug == model.Slug))
+                {
+                    ModelState.AddModelError("", "This slug has already exist");
+                    TempData["edited"] = true;
+                    return View(model);
+                }
+
+                dto.Slug = model.Slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "You has edited the page";
+
+            return RedirectToAction("EditPage");
+        }
+
+        public ActionResult PageDetails(int id)
+        {
+            PageVM model;
+            using (var db = new Db())
+            {
+                var dto = db.Pages.Find(id);
+
+                if (dto == null)
+                {
+                    return Content("That page has not exist.");
+                }
+                model = new PageVM(dto);
+            }
+
+            return View(model);
+        }
     }
 }
