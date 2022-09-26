bf8d77e7b7dc6d2c5ec3a69674cbbc06e7eaf762
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-09-26 17:03:23 +0300

    Добавлены категории

---
+++ MVC_Store/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,99 @@
+﻿using MVC_Store.Models.Data;
+using MVC_Store.Models.ViewModels.Shop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Store.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Shop
+        public ActionResult Categories()
+        {
+            List<CategoryVM> categoryVMList;
+
+            using (var db = new Db())
+            {
+                categoryVMList = db.Categories
+                                    .ToArray()
+                                    .OrderBy(x => x.Sorting)
+                                    .Select(x => new CategoryVM(x))
+                                    .ToList();
+            }
+
+            return View(categoryVMList);
+        }
+
+        //POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            string id;
+
+            using (var db = new Db())
+            {
+                if (db.Categories.Any(c => c.Name == catName))
+                {
+                    return "titletaken";
+                }
+
+                CategoryDTO dto = new CategoryDTO();
+
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 100;
+
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                id = dto.Id.ToString();
+            }
+
+            return id;
+        }
+
+        [HttpPost]
+        public void ReorderCategories(int[] id)
+        {
+            using (var db = new Db())
+            {
+                int count = 1;
+
+                CategoryDTO dto;
+
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+                    db.SaveChanges();
+                    count++;
+                }
+            }
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+
+            using (var db = new Db())
+            {
+                var dto = db.Categories.Find(id);
+
+                if (dto == null)
+                {
+                    return Content("That page has not exist.");
+                }
+
+                db.Categories.Remove(dto);
+                db.SaveChanges();
+
+            }
+
+            TempData["SM"] = "You have deleted a category";
+
+            return RedirectToAction("Categories");
+        }
+    }
+}
--- MVC_Store/Models/Data/Db.cs
+++ MVC_Store/Models/Data/Db.cs
@@ -10,5 +10,6 @@ namespace MVC_Store.Models.Data
     {
         public DbSet<PagesDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
+        public DbSet<CategoryDTO> Categories { get; set; }
     }
 }
